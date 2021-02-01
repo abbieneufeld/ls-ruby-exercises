9018b3dd82fd08510cf14b30300f5078ee10fc30
Abbie Neufeld <abbieneufeld@gmail.com>
2021-02-01 07:01:27 +0000

    Adding exercises from Variables Chapter

---
+++ Variables/age.rb
@@ -0,0 +1,11 @@
+puts "How old are you?"
+age = gets.chomp
+age = age.to_i
+age_in_ten = age + 10
+age_in_twenty = age + 20
+age_in_thirty = age + 30
+age_in_forty = age + 40
+puts "In 10 years you will be #{age_in_ten} years old"
+puts "In 20 years you will be #{age_in_twenty} years old"
+puts "In 30 years you will be #{age_in_thirty} years old"
+puts "In 40 years you will be #{age_in_forty} years old"
