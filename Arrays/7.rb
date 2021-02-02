1c47af8e9ba3f713eb631508be802c1a61648ebb
Abbie Neufeld <abbieneufeld@gmail.com>
2021-02-02 04:18:41 +0000

    Adding exercises from Arrays chapter

---
+++ Arrays/7.rb
@@ -0,0 +1,2 @@
+arr = ["cat", "dog", "horse", "monkey"]
+arr.each_with_index { |val, idx| puts "index: #{idx} value: #{val}"}
---
+++ Arrays/8.rb
@@ -0,0 +1,4 @@
+arr = [1,2,3,4,5]
+arr_two = arr.map {|x| x+2}
+p arr
+p arr_two
