87e47d0ebc0e28fb2aeb3713084bae5f0ff5cb3c
Abbie Neufeld <abbieneufeld@gmail.com>
2021-02-02 06:56:44 +0000

    adding exercises from More Stuff Chapter

---
+++ More-Stuff/exception_example.rb
@@ -0,0 +1,8 @@
+names= ["bob", "joe", "steve", nil, "frank"]
+names.each do |name|
+  begin
+    puts "#{name}'s name has #{name.length} letters"
+  rescue 
+    puts "Something went wrong!"
+  end
+end
